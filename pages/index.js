cf0ca4dc9c61bb9390d8706b84145ad88bb10c13
yshkd <yushi.public@gmail.com>
2023-05-17 19:16:06 +0900

    Reset content style

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
 
         <div className={styles.center}>
           <a className={styles.card1}>
-            <h2 className={inter.className} id="skills"><span className={styles.englishText}>Skills</span></h2>
+            <h2 id="skills"><span className={styles.englishText}>Skills</span></h2>
             <div className={`${styles.cards} ${styles.fadeIn}`}>
               <span className={styles.englishText}> #HTML<br />
                 #CSS<br />
@@ -122,9 +122,9 @@ export default function Home() {
           </ul>
         </aside>
         <footer className={styles.footer}>
-          <p className={styles.footerd}>Contact DM</p>
-          <Link href="https://twitter.com/kaya_awkmiu"><Image className={styles.footern} src='/logo3.png' alt="pick" loading="lazy" position="relative" width="100" height="100"
-          ></Image></Link>
+          <a href="https://twitter.com/kaya_awkmiu">
+          <svg className={styles.footerItem} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
+          </a>
 
         </footer>
       </main>
